e7faa92bccad087b6975106b799871acf6caaa25
duodecanol <metalhwal@gmail.com>
2022-02-21 10:04:25 +0900

    복습1

--- 0.0 CREATE USER SQLD.sql
+++ 0.0 CREATE USER SQLD.sql
@@ -1,4 +1,11 @@
+/**********************************************************************
+ * 2.1 사용자 계정 및 테이블스페이스 생성
+ * 
+ **********************************************************************/
+
+-- Execute the commands by root privilege
 ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
 CREATE USER SQLD IDENTIFIED BY 1234;
 ALTER USER SQLD ACCOUNT UNLOCK;
 GRANT RESOURCE, DBA, CONNECT TO SQLD;
